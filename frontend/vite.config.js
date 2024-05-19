044f168e5f8d12f1743d78ecd2cee280642015e1
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-05-19 12:55:48 +0200

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
-    }
+      '/api': {
+        target: 'http://localhost:5000', // Porta del backend Flask
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // riscrivo senza /api il percorso
+      },
+    },
   },
-  build: {
-    outDir: 'dist'
-  }
-})
+});
