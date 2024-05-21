8ef8392fad97f745b501705ee85b77857d8f01b5
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-05-21 19:57:17 +0200

    add Utente and query by privacy by design

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import urllib.parse # Questo decodifica qualsiasi carattere codificato nel parametro URL.
 import db
 
 app = Flask(__name__)
@@ -9,13 +10,19 @@ CORS(app)
 def homePage():
     return db.getPrivacyByDesign()
 
-@app.route('/api/posd-knowledge-base')
+@app.route('/api/pattern')
 def showDB():
     return db.getPosdKnowledgeBase()
 
-@app.route('/api/posd-knowledge-base/strategies=<string:strategies>', methods=['GET'])
+@app.route('/api/pattern/strategies=<string:strategies>', methods=['GET'])
 def patternsByStrategies(strategies):
     return db.getArticleStrategies(strategies)
+
+@app.route('/api/pattern/privacybydesign=<string:privacybydesign>', methods=['GET'])
+def privacyByDesign(privacybydesign):
+    decoded_privacybydesign = urllib.parse.unquote(privacybydesign)
+    print(decoded_privacybydesign)  # Stampa il valore decodificato per il debug
+    return db.getPatternByPrivacyByDesign(decoded_privacybydesign)
 
 if __name__ == '__main__':
     app.run()
