d6992c807dd65f478592182aed196236eba59123
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-05-23 13:11:19 +0200

    Update Contatti.jsx

--- frontend/src/pages/Component/Contatti.jsx
+++ frontend/src/pages/Component/Contatti.jsx
@@ -1,31 +1,25 @@
-
-function Contatti(){
+function Contatti() {
     return (
         <div className="container mx-auto mt-5 p-6 flex justify-center">
             <div className="grid grid-cols-3 gap-6">
-
-                <div className={`bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer `}>
-
+                <div className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer">
                     <h2 className="text-xl font-bold">Lorenzo Calabrese</h2>
                     <p className="mt-2">Frontend developer</p>
-                    <p>Email di lavoro: l.calabrese28@studenti.uniba.it</p>
-
+                    <p>Email: <a href="mailto:l.calabrese28@studenti.uniba.it" className="text-blue-500 hover:underline">l.calabrese28@studenti.uniba.it</a></p>
                 </div>
-                <div className={`bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer `}>
+                <div className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer">
                     <h2 className="text-xl font-bold">Francesco Conforti</h2>
                     <p className="mt-2">Backend developer</p>
-                    <p>Email di lavoro: f.conforti9@studenti.uniba.it</p>
+                    <p>Email: <a href="mailto:f.conforti9@studenti.uniba.it" className="text-blue-500 hover:underline">f.conforti9@studenti.uniba.it</a></p>
                 </div>
-
-                <div className={`bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer `}>
+                <div className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer">
                     <h2 className="text-xl font-bold">Giuseppe Pio De Biase</h2>
                     <p className="mt-2">Frontend developer</p>
-                    <p>Email di lavoro: g.debiase5@studenti.uniba.it</p>
+                    <p>Email: <a href="mailto:g.debiase5@studenti.uniba.it" className="text-blue-500 hover:underline">g.debiase5@studenti.uniba.it</a></p>
                 </div>
             </div>
         </div>
-
     );
 }
 
-export default Contatti
+export default Contatti;
