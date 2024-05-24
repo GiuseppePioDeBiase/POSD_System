f156e732e337a8897882d0e753180d5aedee7fba
Giuseppe Pio De Biase <giuseppepiodebiase@yahoo.it>
2024-05-24 18:11:14 +0200

    Fix Navbar+Searchbar
    Aggiustati errori nel codice
    
    Bisogna Aggiustare la navabar(bisogna Centrarla)

--- frontend/src/pages/Component/Searchbar/Searchbar.jsx
+++ frontend/src/pages/Component/Searchbar/Searchbar.jsx
@@ -1,16 +1,14 @@
-/* Searchbar.jsx */
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Searchbar() {
   return (
-    <div className="container d-flex justify-content-center align-items-center">
+    <div className="container d-flex justify-content-center align-items-center vh-100">
       <div className="input-group col-sm-6 input-group-lg">
         <div className="input-group-prepend">
           <span className="input-group-text logo">
             <Link to={"/"}>
-               <img src="images/logo.png" alt="Logo"/>
+              <img src="images/logo.png" alt="Logo" />
             </Link>
-
           </span>
         </div>
         <input type="text" className="form-control" />
