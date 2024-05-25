b55d49523769c282f5e10e1f7ad4a4d9fe997503
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-05-25 16:04:31 +0200

    Fix NotFound.css

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCog } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import './NotFound.css';
 
 // Aggiungi l'icona al registro di libreria
 library.add(faCog);
@@ -10,7 +11,7 @@ const NotFound = () => {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 id="bigNumber" className="text-9xl">
         <span className="num">4</span>
-        <FontAwesomeIcon icon="cog" spin />
+        <FontAwesomeIcon icon="cog" spin className="icon-spin" />
         <span className="num">4</span>
       </h1>
       <h2 id="notFoundText" className="text-3xl font-bold mt-8">
