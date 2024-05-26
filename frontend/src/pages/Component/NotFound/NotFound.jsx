c4de22ce44cc7b1d07687c662af186c480f8b28a
LorenzoCalabrese03 <151526103+LorenzoCalabrese03@users.noreply.github.com>
2024-05-26 16:26:14 +0200

    responsive notfound

--- frontend/src/pages/Component/NotFound/NotFound.jsx
+++ frontend/src/pages/Component/NotFound/NotFound.jsx
@@ -10,13 +10,13 @@ library.add(faCog);
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 id="bigNumber" className="text-9xl">
+      <h1 id="bigNumber" className="text-5xl md:text-9xl">
         <span className="num">4</span>
         <FontAwesomeIcon icon="cog" spin className="icon-spin" />
         <span className="num">4</span>
       </h1>
-      <h2 id="notFoundText" className="text-3xl font-bold mt-8">
-        <span className="text-xl-center">Page Not Found</span>
+      <h2 id="notFoundText" className="text-xl md:text-3xl font-bold mt-8">
+        <span className="text-center">Page Not Found</span>
       </h2>
       <Link to="/">
         <button className="mt-8 px-4 py-2 bg-blue-500 text-white rounded hover:bg-black-700">
