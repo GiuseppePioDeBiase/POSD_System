e11f2e43a0ac42c171f1745e565e48898e3e3c28
Giuseppe Pio De Biase <giuseppepiodebiase@yahoo.it>
2024-05-28 11:12:56 +0200

    Back commit kekko
    kekko caramello

--- frontend/src/pages/Component/Navbar/NavBar.jsx
+++ frontend/src/pages/Component/Navbar/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar() {
 
   const handleMenuItemClick = (index) => {
     setActiveMenuItem(index);
-    if (window.innerWidth <= 768) { // Controlla se la larghezza della finestra è inferiore o uguale a 768px (dispositivi mobili)
+    if (window.innerWidth <= 2230) { // Controlla se la larghezza della finestra è inferiore o uguale a 768px (dispositivi mobili)
       setMenuOpen(false); // Chiudi la navbar solo se la larghezza della finestra è inferiore o uguale a 768px
     }
   };
@@ -55,19 +55,19 @@ function NavBar() {
               </li>
             ))}
           </div>
-          <div className="bottom">
+          <div className="bottom absolute bottom-0 w-full">
             {bottomItems.map((item, index) => (
-              <li key={index} onClick={() => handleMenuItemClick(menuItems.length + index)}>
+              <li key={index} className="bottom-item" style={{ '--bg': '#100' }}>
                 <Link to={item.to} className="flex items-center">
-                  {item.icon === "img" ? (
-                    <div className="imgBx flex justify-center items-center min-w-10 h-10">
-                      <img src={item.imgSrc} alt={item.alt} />
-                    </div>
-                  ) : (
-                    <div className="icon flex justify-center items-center min-w-16 h-20">
+                  <div className="icon flex justify-center items-center min-w-16 h-20">
+                    {item.icon === "img" ? (
+                      <div className="imgBx w-10 h-10 rounded-full overflow-hidden">
+                        <img src={item.imgSrc} alt={item.alt} className="w-full h-full object-cover" />
+                      </div>
+                    ) : (
                       <ion-icon name={item.icon}></ion-icon>
-                    </div>
-                  )}
+                    )}
+                  </div>
                   <div className="text text-lg">{item.text}</div>
                 </Link>
               </li>
--- frontend/src/pages/Component/PagineMenu/Contatti.jsx
+++ frontend/src/pages/Component/PagineMenu/Contatti.jsx
@@ -1,25 +1,26 @@
 function Contatti() {
-  return (
-    <div className="container mx-auto mt-5 p-4 lg:p-6">
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-6">
-        <div className="bg-white shadow-md rounded-lg p-4 lg:p-6 hover:bg-gray-100 cursor-pointer">
-          <h2 className="text-xl lg:text-2xl font-bold">Lorenzo Calabrese</h2>
-          <p className="mt-2">Frontend developer</p>
-          <p>Email: <a href="mailto:l.calabrese28@studenti.uniba.it" className="text-blue-500 hover:underline">l.calabrese28@studenti.uniba.it</a></p>
+    return (
+        <div className="container mx-auto mt-5 p-6 flex justify-center">
+            <div className="grid grid-cols-3 gap-6">
+                <div className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer">
+
+                    <h2 className="text-xl font-bold">Lorenzo Calabrese</h2>
+                    <p className="mt-2">Frontend developer</p>
+                    <p>Email: <a href="mailto:l.calabrese28@studenti.uniba.it" className="text-blue-500 hover:underline">l.calabrese28@studenti.uniba.it</a></p>
+                </div>
+                <div className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer">
+                    <h2 className="text-xl font-bold">Francesco Conforti</h2>
+                    <p className="mt-2">Backend developer</p>
+                    <p>Email: <a href="mailto:f.conforti9@studenti.uniba.it" className="text-blue-500 hover:underline">f.conforti9@studenti.uniba.it</a></p>
+                </div>
+                <div className="bg-white shadow-md rounded-lg p-4 hover:bg-gray-100 cursor-pointer">
+                    <h2 className="text-xl font-bold">Giuseppe Pio De Biase</h2>
+                    <p className="mt-2">Frontend developer</p>
+                    <p>Email: <a href="mailto:g.debiase5@studenti.uniba.it" className="text-blue-500 hover:underline">g.debiase5@studenti.uniba.it</a></p>
+                </div>
+            </div>
         </div>
-        <div className="bg-white shadow-md rounded-lg p-4 lg:p-6 hover:bg-gray-100 cursor-pointer">
-          <h2 className="text-xl lg:text-2xl font-bold">Francesco Conforti</h2>
-          <p className="mt-2">Backend developer</p>
-          <p>Email: <a href="mailto:f.conforti9@studenti.uniba.it" className="text-blue-500 hover:underline">f.conforti9@studenti.uniba.it</a></p>
-        </div>
-        <div className="bg-white shadow-md rounded-lg p-4 lg:p-6 hover:bg-gray-100 cursor-pointer">
-          <h2 className="text-xl lg:text-2xl font-bold">Giuseppe Pio De Biase</h2>
-          <p className="mt-2">Frontend developer</p>
-          <p>Email: <a href="mailto:g.debiase5@studenti.uniba.it" className="text-blue-500 hover:underline">g.debiase5@studenti.uniba.it</a></p>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Contatti;
