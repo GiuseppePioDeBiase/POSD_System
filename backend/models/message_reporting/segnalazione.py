e312e4c5ad7e1a5a92f37110736f8140d376dfa6
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-06-04 16:12:00 +0200

    Classe Segnalazione

--- backend/models/message_reporting/segnalazione.py
+++ backend/models/message_reporting/segnalazione.py
@@ -3,25 +3,34 @@ from backend.models.message_reporting.base_message import BaseMessage
 from flask import request, jsonify
 
 db = conn_db()
-segnalazioneCollection = db['SegnalazioneCISO']
+segnalazioneCollection = db['Segnalazione']
 
 
 class Segnalazione(BaseMessage):
-    def __init__(self, oggetto, messaggio, ip_pubblico):
+    def __init__(self, oggetto, messaggio, ip_pubblico, mail):
         super().__init__(oggetto, messaggio, ip_pubblico)
+        self.mail = mail
 
     @classmethod
-    def insertSegnalazione(cls):
+    def insertSegnalazione(cls, mail):
         dati = request.json
 
         if not cls.validate(dati.get('oggetto', ''), dati.get('messaggio', '')):
-            return jsonify({"successo": False, "messaggio": "SegnalazioneCISO non valida!"}), 400
+            return jsonify({"successo": False, "messaggio": "Segnalazione non valida!"}), 400
 
         segnalazione = cls(
             oggetto=dati['oggetto'],
             messaggio=dati['messaggio'],
-            ip_pubblico=request.remote_addr
+            ip_pubblico=request.remote_addr,
+            mail=mail
         )
 
         segnalazioneCollection.insert_one(segnalazione.to_json())
-        return jsonify({"successo": True, "messaggio": "SegnalazioneCISO ricevuta!"}), 201
+        return jsonify({"successo": True, "messaggio": "Segnalazione ricevuta!"}), 201
+
+    def to_json(self):
+        base_json = super().to_json()
+        base_json.update({
+            "mail": self.mail
+        })
+        return base_json
