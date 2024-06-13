855d7a853143f54173e420e8299509135a8ee26b
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-06-14 01:29:27 +0200

    fix sonar

--- frontend/src/pages/Component/Componenti globali/useToken.jsx
+++ frontend/src/pages/Component/Componenti globali/useToken.jsx
@@ -1,23 +1,29 @@
-import { useState } from 'react';
+import {useState} from 'react';
 
 function useToken() {
+    // Funzione per ottenere il token dalla sessionStorage
     const getToken = () => sessionStorage.getItem('token');
-    const [token, setTokenState] = useState(getToken());
 
-    const setToken = (userToken) => {
+    // Utilizzo di useState per gestire lo stato del token
+    const [token, setToken] = useState(() => getToken()); // Utilizzo di setToken invece di setTokenState
+
+    // Funzione per impostare il token nella sessionStorage e nello stato locale
+    const setTokenValue = (userToken) => {
         sessionStorage.setItem('token', userToken);
-        setTokenState(userToken);
+        setToken(userToken); // Utilizzo di setToken per aggiornare lo stato
     };
 
+    // Funzione per rimuovere il token dalla sessionStorage e impostare lo stato a null
     const removeToken = () => {
         sessionStorage.removeItem('token');
-        setTokenState(null);
+        setToken(null); // Imposta il token a null nello stato locale utilizzando setToken
     };
 
+    // Ritorna l'oggetto con il token, la funzione per impostare il token e la funzione per rimuovere il token
     return {
-        token: token, // destrutturo il valore di stato 'token'
-        setToken: setToken, // destrutturo la funzione setter 'setToken'
-        removeToken: removeToken
+        token, // Utilizzo della destrutturazione per il valore di stato 'token'
+        setToken: setTokenValue, // Utilizzo della funzione setTokenValue invece di setToken
+        removeToken // Restituzione della funzione per rimuovere il token
     };
 }
 
