79d0a06da107360444867edcc1c221083d8de972
checcoconf <82725386+checcoconf@users.noreply.github.com>
2024-06-13 12:45:56 +0200

    Fix feedback

--- backend/models/message_reporting/feedback.py
+++ backend/models/message_reporting/feedback.py
@@ -1,3 +1,4 @@
+from backend.models.attors.ruolo import Ruolo
 from backend.models.message_reporting.base_message import BaseMessage
 from flask import jsonify, request
 from backend.config.db import conn_db
@@ -8,19 +9,35 @@ feedbackCollection = db['InserisciFeedback']
 
 class Feedback(BaseMessage):
     def __init__(self, oggetto, messaggio, mail):
-        super().__init__(oggetto, messaggio, mail)
+        super().__init__(oggetto, messaggio)
+        self.mail = mail
 
     @classmethod
-    def insertFeedback(cls): # Inserire la mail quando viene rilasciato un feedback
+    def insertFeedback(cls, mail):
+        from backend.models.attors.utente import utenti
+        utente = utenti.find_one({"email": mail})
+        if not utente or utente['ruolo'] != Ruolo.UTENTE.value:
+            return jsonify({"successo": False,
+                            "messaggio": "L'utente non esiste o non ha i privilegi necessari per visualizzare le "
+                                         "segnalazioni."}), 403
+
         dati = request.json
 
         if not cls.validate(dati.get('oggetto', ''), dati.get('messaggio', '')):
-            return jsonify({"successo": False, "messaggio": "InserisciFeedback non valido!"}), 400
+            return jsonify({"successo": False, "messaggio": "Feedback non valido!"}), 400
 
         feedback = cls(
             oggetto=dati['oggetto'],
             messaggio=dati['messaggio'],
+            mail=mail
         )
 
         feedbackCollection.insert_one(feedback.to_json())
-        return jsonify({"successo": True, "messaggio": "InserisciFeedback ricevuto!"}), 201
+        return jsonify({"successo": True, "messaggio": "Feedback ricevuto!"}), 201
+
+    def to_json(self):
+        return {
+            'oggetto': self.oggetto,
+            'messaggio': self.messaggio,
+            'mail': self.mail
+        }
--- backend/routes/feedback_routes.py
+++ backend/routes/feedback_routes.py
@@ -1,9 +1,12 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required, get_jwt_identity
+
 from backend.models.message_reporting.feedback import Feedback
 
 feedback_bp = Blueprint('feedback_bp', __name__)
 
 
 @feedback_bp.route('/api/feedback', methods=['POST'])
+@jwt_required()
 def add_feedback():
-    return Feedback.insertFeedback()
+    return Feedback.insertFeedback(mail=get_jwt_identity())
